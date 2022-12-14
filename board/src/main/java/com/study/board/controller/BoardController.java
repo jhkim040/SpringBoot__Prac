bbeedf35e7cd8fcac873a96d12d12a42d18c4ce8
jh kim <pjhkim0408@gmail.com>
2022-12-14 16:32:12 +0900

    view post list & update/delete posts

--- board/src/main/java/com/study/board/controller/BoardController.java
+++ board/src/main/java/com/study/board/controller/BoardController.java
@@ -4,7 +4,9 @@ import com.study.board.entity.Board;
 import com.study.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class BoardController {
     @Autowired
     private BoardService boardService;
+
+    // 글 작성
     @GetMapping("/board/write") // localhost:8090/board/write
     public String boardWriteForm() {
         return "boardWrite";
@@ -19,6 +23,41 @@ public class BoardController {
     @PostMapping("/board/writepro")
     public String boardWritePro(Board board) {
         boardService.write(board);
-        return "";
+        return "redirect:/board/list";
+    }
+
+    // 게시글 리스트
+    @GetMapping("/board/list")
+    public String boardList(Model model) {
+        model.addAttribute("list", boardService.boardList());
+        return "boardList";
+    }
+
+    // 게시글 상세
+    @GetMapping("/board/view") // localhost:8090/board/view?id=1
+    public String boardView(Model model, Integer id) {
+    model.addAttribute("board", boardService.boardView(id));
+       return "boardView";
+    }
+
+    @GetMapping("/board/delete")
+    public String boardDelete(Integer id) {
+        boardService.boardDelete(id);
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("/board/modify/{id}")
+    public String boardModify(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("board", boardService.boardView(id));
+        return "boardModify";
+    }
+
+    @PostMapping("/board/update/{id}")
+    public String boardModify(@PathVariable("id") Integer id, Board board){
+        Board boardTemp = boardService.boardView(id);
+        boardTemp.setTitle(board.getTitle());
+        boardTemp.setContent(board.getContent());
+        boardService.write(boardTemp);
+        return "redirect:/board/list";
     }
 }
--- board/src/main/java/com/study/board/service/BoardService.java
+++ board/src/main/java/com/study/board/service/BoardService.java
@@ -4,6 +4,9 @@ import com.study.board.entity.Board;
 import com.study.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import java.util.List;
 
 @Service
 public class BoardService {
@@ -15,4 +18,15 @@ public class BoardService {
         boardRepository.save(board);
     }
 
+    public List<Board> boardList() {
+        return boardRepository.findAll();
+    }
+
+    public Board boardView(Integer id) {
+        return boardRepository.findById(id).get();
+    }
+
+    public void boardDelete(Integer id) {
+        boardRepository.deleteById(id);
+    }
 }
