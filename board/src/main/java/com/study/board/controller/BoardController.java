24e6fcafebabe1d89a543e5f6280ffc8dfa47331
jh kim <pjhkim0408@gmail.com>
2022-12-15 15:13:51 +0900

    paging posts & search posts by keywords

--- board/src/main/java/com/study/board/controller/BoardController.java
+++ board/src/main/java/com/study/board/controller/BoardController.java
@@ -3,6 +3,10 @@ package com.study.board.controller;
 import com.study.board.entity.Board;
 import com.study.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,15 +25,39 @@ public class BoardController {
         return "boardWrite";
     }
     @PostMapping("/board/writepro")
-    public String boardWritePro(Board board) {
+    public String boardWritePro(Board board, Model model) {
         boardService.write(board);
-        return "redirect:/board/list";
+
+        model.addAttribute("message", "글 작성이 완료되었습니다.");
+        // model.addAttribute("message", "글 작성이 실패하였습니다.");
+        model.addAttribute("searchUrl", "/board/list");
+        return "message";
     }
 
     // 게시글 리스트
-    @GetMapping("/board/list")
-    public String boardList(Model model) {
-        model.addAttribute("list", boardService.boardList());
+    @GetMapping("/board/list") // page : default page, size : 한 페이지 게시글 개수, sort : 정렬 기준 컬럼, direction : 정렬 순서
+    public String boardList(Model model,
+                            @PageableDefault(page=0, size=10, sort="id", direction = Sort.Direction.DESC) Pageable pageable,
+    String searchKeyword) {
+
+        Page<Board> list = null;
+        if(searchKeyword == null) { // 검색 아닐 때
+           list = boardService.boardList(pageable);
+        } else { // 검색일 때
+            list = boardService.boardSearchList(searchKeyword, pageable);
+        }
+
+        // nowPage : 현재 페이지
+        // startPage : 블럭에서 보여줄 시작 페이지
+        // endPage : 블럭에서 보여줄 마지막 페이지
+        int nowPage = list.getPageable().getPageNumber() + 1; // pageable의 페이지는 0에서 시작
+        int startPage = Math.max(nowPage - 4, 1);
+        int endPage = Math.min(nowPage + 5, list.getTotalPages());
+
+        model.addAttribute("list", list);
+        model.addAttribute("nowPage", nowPage);
+        model.addAttribute("startPage",startPage);
+        model.addAttribute("endPage", endPage);
         return "boardList";
     }
 
@@ -53,11 +81,15 @@ public class BoardController {
     }
 
     @PostMapping("/board/update/{id}")
-    public String boardModify(@PathVariable("id") Integer id, Board board){
+    public String boardModify(@PathVariable("id") Integer id, Board board, Model model){
         Board boardTemp = boardService.boardView(id);
         boardTemp.setTitle(board.getTitle());
         boardTemp.setContent(board.getContent());
         boardService.write(boardTemp);
-        return "redirect:/board/list";
+
+        model.addAttribute("message", "글 수정이 완료되었습니다.");
+        // model.addAttribute("message", "글 수정이 실패하였습니다.");
+        model.addAttribute("searchUrl", "/board/list");
+        return "message";
     }
 }
--- board/src/main/java/com/study/board/repository/BoardRepository.java
+++ board/src/main/java/com/study/board/repository/BoardRepository.java
@@ -1,10 +1,16 @@
 package com.study.board.repository;
 
 import com.study.board.entity.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Integer> {
+
+    // findBy(Column) : 해당 컬럼 검색
+    // findBy(Column)Containing : 키워드 검색
+    Page<Board> findByTitleContaining(String searchKeyword, Pageable pageable);
 
 }
